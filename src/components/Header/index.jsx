aaae799d6dbabd46870e523bce1ca2a4c5a04f5b
Caio Paiva <caiowick@hotmail.com>
2025-03-28 20:55:43 -0300

    adding global styles

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import { styled } from "styled-components"
+
+const HeaderStyled = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img {
+        max-width: 212px;
+    }
+`
+
+const Header = () => {
+    return (<HeaderStyled>
+        <img src="images/logo.png" alt="Logo" />
+    </HeaderStyled>)
+}
+
+export default Header
